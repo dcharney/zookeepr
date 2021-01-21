8237067f6e4a044754694b28f124e89bc827106a
dcharney <delaneycharney@gmail.com>
2021-01-21 22:48:41 +0100

    search by id working

--- server.js
+++ server.js
@@ -56,7 +56,11 @@ app.get('/api/animals', (req, res) => {
 
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
-        res.json(result);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
 });
 
 
