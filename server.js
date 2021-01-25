5ec935f495f5a2fcd414c28c03c94b21802d3af7
dcharney <delaneycharney@gmail.com>
2021-01-25 22:38:19 +0100

    existing html loading with references working

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
+
 const {animals} = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
