3cee2545eaae55cf44051a8697c83eba7141b1a1
dcharney <delaneycharney@gmail.com>
2021-01-27 21:07:14 +0100

    app routes added

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+const { response } = require("express");
+
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+
+router.use(animalRoutes);
+
+module.exports = router;
